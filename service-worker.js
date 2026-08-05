c7eeb2e3f173c4de1bf6f9ac3f693c6340dc61e5
Drivers Mate <agentob077-cpu@users.noreply.github.com>
2026-08-05 21:04:08 +0100

    fix: refresh offline cache for Gi timesheet

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'drivers-mate-personal-log-v1';
+const CACHE_NAME = 'drivers-mate-personal-log-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
